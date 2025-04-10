1641c1abb580397e7a77bc25fc6137dcfd45feef
Sam <alvasvart.de@gmail.com>
2025-04-10 11:33:40 +0200

    feat: add step one process due to uri change, update readme

--- AppointmentScraper.py
+++ AppointmentScraper.py
@@ -1,21 +1,42 @@
 import time
 import datetime
 import requests
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.service import Service   # Change chrome to firefox
+from selenium.webdriver.chrome.options import Options   # Change chrome to firefox
 from bs4 import BeautifulSoup
 from plyer import notification
 
-# URI der Terminbuchung
-URL = "https://www.terminland.de/noris-psychotherapie/online/ADHS_new/default.aspx?m=39059&ll=HT7kc&dpp=HIqZ2&dlgid=9&step=3&dlg=1&a2291013099=2291025408&css=1"
-
-# Hinweistext
+# Info text
 INFO = "Für die ADHS-Abklärung stehen derzeit keine freien Termine zur Verfügung."
 
-# Zeitintervall (in Sekunden)
+# Time intervall (in Seconds)
 INTERVALL = 600
 
-def check_termine():
+def check_dates():
+
+    # Setup for Chrome
+    options = Options()
+    options.add_argument("--headless")  # No browser window popping up
+    driver = webdriver.Chrome(service=Service("D:\dev\Chromedriver\chromedriver-win64\chromedriver.exe"), options=options)  # Change Chrome to Firefox
+
+    driver.get("https://www.terminland.de/noris-psychotherapie/online/ADHS_new/")
+    time.sleep(5)   # Page loading takes a while
+
+    # Check radio button
+    radio_btn = driver.find_element(By.CLASS_NAME, "checkmark")
+    radio_btn.click()
+
+    # Check next button
+    next_btn = driver.find_element(By.ID, "btnGo")
+    next_btn.click()
+
+    time.sleep(7)   # Page loading takes even longer sometimes while checking dates
+    url = driver.current_url
+
     try:
-        response = requests.get(URL)
+        response = requests.get(url)
         response.raise_for_status()
         soup = BeautifulSoup(response.text, "html.parser")
 
@@ -32,9 +53,11 @@ def check_termine():
             print(datetime.datetime.now().strftime("%d.%m.%Y %H:%M"),"Kein <div class='card-body'> gefunden. Terminpicker verfügbar?")
             notification.notify(title="Something changed", message="Termine verfügbar?", timeout=20)
 
+        driver.quit()
+
     except Exception as e:
-        print(f"Fehler: {e}")
+        print(f"Error: {e}")
 
 while True:
-    check_termine()
+    check_dates()
     time.sleep(INTERVALL)
--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 # appointment-scraper
 
-Dies wird eine Anwendung, die mir helfen soll, endlich einen fucking ADHS-Diagnosetermin zu bekommen.
+This application checks one specific appointment page for open appointments.
+
+Download:
+> Chromedriver https://developer.chrome.com/docs/chromedriver/downloads?hl=de for Chrome
+> 
+> Geckodriver https://github.com/mozilla/geckodriver/releases for Firefox
+
+run with `python AppointmentScraper.py` from the file directory
